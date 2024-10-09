aed473388c121d4c9a522a96dc574e2dd3963c15
Robertkevyngg <robertkevyngg>
2024-10-09 09:08:39 -0300

    atu

--- front/js/script.js
+++ front/js/script.js
@@ -3,8 +3,17 @@ const baseURL = 'localhost:3000';
 const filmeEndPoint = '/filmes';
 
 async function obterFilmes() {
-    const URLcompleta = '${protocolo}${baseURL}${filmeEndPoint}';
+    const URLcompleta = `${protocolo}${baseURL}${filmeEndPoint}`;
     const filmes = (await axios.get(URLcompleta)).data
+    let tabela = document.querySelector('.filmes');
+    let corpotabela = tabela.getElementsByTagName('tbody')[0];
+    for (let filme of filmes) {
+        let linha = corpotabela.insertRow();
+        let celulatitulo = linha.insertCell();
+        let celulasinopse = linha.insertCell();
+        celulatitulo.innerHTML = filme.titulo;
+    
+    }
 
     console.log(filmes);
 }
